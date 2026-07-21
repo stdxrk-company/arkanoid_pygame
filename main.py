a4bbd9c86a5114d438696a0337bf15c7720cc261
Anton Ilchenko <anton_ilchenko@epam.com>
2026-07-21 11:23:22 +0200

    FEAT: All Ball collision Detection

--- main.py
+++ main.py
@@ -3,6 +3,36 @@ import settings as cfg
 from screens.game_screen import run as game_screen
 from game.entities import Paddle, Brick, Ball
 
+def _bounce_off_rect(ball: Ball, rect: pygame.Rect):
+    """ Checks if the Ball collides with the given rect. """
+
+    # Calculate ball's overlaps and find the smallest one
+    overlap_left = ball.rect.right - rect.left
+    overlap_right = rect.right - ball.rect.left
+    overlap_top = ball.rect.bottom - rect.top
+    overlap_bottom = rect.bottom - ball.rect.top
+
+    min_overlap = min(
+        overlap_bottom,
+        overlap_left,
+        overlap_right,
+        overlap_top)
+    
+    # Calculate the Ball's final velocities
+    if min_overlap == overlap_top and ball.vy > 0:
+        ball.rect.bottom = rect.top
+        ball.vy *= -1
+    elif min_overlap == overlap_bottom and ball.vy < 0:
+        ball.rect.top = rect.bottom
+        ball.vy *= -1
+    elif min_overlap == overlap_left and ball.vx > 0:
+        ball.rect.right = rect.left
+        ball.vx *= -1
+    elif min_overlap == overlap_right and ball.vy < 0:
+        ball.rect.left = rect.right
+        ball.vx *= -1
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((cfg.WIDTH, cfg.HEIGHT))
@@ -34,6 +64,7 @@ def main():
         ball.draw(screen)
 
         for brick in bricks:
+            _bounce_off_rect(ball, brick.rect)
             brick.draw(screen)
 
         for event in pygame.event.get():
